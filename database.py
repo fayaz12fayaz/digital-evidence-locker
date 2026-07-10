90e2c482bf7784d43b306d1c646a7521d1e741bc
Fayaz <131447521+fayaz12fayaz@users.noreply.github.com>
2026-07-10 20:51:57 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 import mysql.connector
 class DataBase_Connection:
-    def connect_db(self):
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="password",
-            database="digital_evidence_locker"
-        )
-        return conn
+    try:
+        def connect_db(self):
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="your_password",
+                database="digital_evidence_locker"
+            )
+            return conn
+    except mysql.connector.Error as err:
+        print(err)
